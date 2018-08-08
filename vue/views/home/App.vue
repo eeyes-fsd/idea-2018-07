3614c87113cf0a1155acf93a2718d54b91f672dd
finntenzor <finntenzor@gmail.com>
2018-08-08 18:15:50 +0800

    添加社团登录

--- vue/views/home/App.vue
+++ vue/views/home/App.vue
@@ -2,14 +2,17 @@
   <div>
     <header>
       <button @click="userLogin">个人登录</button>
-      <button>社团登录</button>
-      <p>{{ errorMessage }}</p>
+      <input type="text" v-model="org.email" placeholder="邮箱地址">
+      <input type="text" v-model="org.password" placeholder="密码">
+      <button @click="orgLogin">社团登录</button>
+      <p>您当前的登陆身份类型：{{ loginType }}</p>
+      <p>错误信息：{{ errorMessage }}</p>
     </header>
   </div>
 </template>
 
 <script>
-import requests from '@/api/requests.js'
+import requests, { getLoginType } from '@/api/requests.js'
 import Example from '@/components/Example'
 export default {
   components: {
@@ -17,7 +20,11 @@ export default {
   },
   data () {
     return {
-      errorMessage: '暂时没有错误发生'
+      org: {
+        email: '',
+        password: ''
+      },
+      errorMessage: '暂时没有错误发生',
     }
   },
   methods: {
@@ -28,6 +35,24 @@ export default {
       } catch (err) {
         this.errorMessage = err.message || '未知错误'
       }
+    },
+    async orgLogin () {
+      try {
+        let data = await requests.post('/organizations/authorizations', this.org)
+        location.href = data.url
+      } catch (err) {
+        this.errorMessage = err.message || '未知错误'
+      }
+    },
+  },
+  computed: {
+    loginType() {
+      const trans = {
+        'null': '未登录',
+        'user': '个人用户',
+        'org': '社团'
+      }
+      return trans[getLoginType()] || '错误的登陆身份类型'
     }
   }
 }

98009638656c875e70e6870b33c0528afdf87b98
finntenzor <finntenzor@gmail.com>
2018-08-08 17:44:26 +0800

    测试个人登录

--- vue/views/home/App.vue
+++ vue/views/home/App.vue
@@ -1,12 +1,34 @@
 <template>
-  <Example />
+  <div>
+    <header>
+      <button @click="userLogin">个人登录</button>
+      <button>社团登录</button>
+      <p>{{ errorMessage }}</p>
+    </header>
+  </div>
 </template>
 
 <script>
+import requests from '@/api/requests.js'
 import Example from '@/components/Example'
 export default {
   components: {
     Example
+  },
+  data () {
+    return {
+      errorMessage: '暂时没有错误发生'
+    }
+  },
+  methods: {
+    async userLogin () {
+      try {
+        let data = await requests.get('/api/users/authorizations')
+        location.href = data.url
+      } catch (err) {
+        this.errorMessage = err.message || '未知错误'
+      }
+    }
   }
 }
 </script>

77723ca72999f7a15eeb8c76cbb75ccef8d74517
XuZhixuan <sxxuzhixuan@hotmail.com>
2018-08-14 12:06:19 +0800

    添加分类转换器

---
+++ app/Transformers/CategoryTransformer.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Transformers;
+
+use App\Models\Category;
+use League\Fractal\TransformerAbstract;
+
+class CategoryTransformer extends TransformerAbstract
+{
+    public function transform(Category $category)
+    {
+        //
+    }
+}
